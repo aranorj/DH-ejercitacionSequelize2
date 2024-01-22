049e66dbcb6e3df08f1497e3ca54fcef42cc323c
diegomper <dmper9111@gmail.com>
2024-01-21 23:47:26 -0300

    Delete Movie

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -5,12 +5,12 @@ const moviesController = require('../controllers/moviesController');
 router.get('/movies', moviesController.list);
 //router.get('/movies/new', moviesController.new);
 //router.get('/movies/recommended', moviesController.recomended);
-router.get('/movies/:id/detail', moviesController.getOne);
+router.get('/movies/detail/:id', moviesController.getOne);
 
 router.get('/movies/:id', moviesController.edit);
 
 router.put('/movies/:id', moviesController.update);
 
-//router.delete(/movies/:id', moviesController.delete);
+router.delete('/movies/:id', moviesController.delete);
 
 module.exports = router;
